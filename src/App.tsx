bef393ee030e2df9a1c881ac90cfe1e429a00bb7
Antonio Alves <antoniodecastroalves@gmail.com>
2020-05-30 00:34:34 -0300

    Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import SignIn from './pages/Signin';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <h1>Hello World</h1>
+    <SignIn />
     <GlobalStyle />
   </>
 );
