02912fd8d305ad433efbf5f39451fdfcaf2d6253
Antonio Alves <antoniodecastroalves@gmail.com>
2020-06-15 23:52:06 -0300

    Registration page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import SignIn from './pages/Signin';
+// import SignIn from './pages/Signin';
+import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );
